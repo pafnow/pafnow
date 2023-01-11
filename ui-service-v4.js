7083ad8a7ffcc77241d3cdc2696065018e91b791
pafnow <pafnow@hotmail.com>
2023-01-11 10:38:54 +0100

    Create ui-service-v4.js

---
+++ ui-service-v4.js
@@ -0,0 +1,10 @@
+odoo.define('@web/file_a', function (require) {
+'use strict';
+let __exports = {};
+
+const { SIZES } = require("@web/core/ui/ui_service");
+
+SIZES.XXL = 10;
+alert("Hello world!");
+
+});
